3d3352c6db6659fb425683f0fa195bd9e8acfd18
Mitchell Christ <xzmaru@gmail.com>
2022-06-21 22:42:21 -0700

    refactor a bit

--- src/lib/AudioCanvas.svelte
+++ src/lib/AudioCanvas.svelte
@@ -1,58 +1,24 @@
 <audio bind:this={audio} controls autoplay />
 
-<Frequency />
+<Frequency {analyzer} />
 
 <script>
-	import { onMount } from 'svelte'
-	import Frequency, { canvas } from './Frequency.svelte'
+	import Frequency from './Frequency.svelte'
 
 	export let stream
 
-	let audio
-	let analyzer
-	let frame
+	let audio, analyzer
 
 	$: if (audio && audio?.srcObject !== stream) {
 		audio.srcObject = stream
 
 		let audioCtx = new AudioContext()
+
 		analyzer = audioCtx.createAnalyser()
-		let source = audioCtx.createMediaElementSource(audio)
-
-		source.connect(analyzer)
 		analyzer.connect(audioCtx.destination)
+
+		audioCtx
+			.createMediaElementSource(audio)
+			.connect(analyzer)
 	}
-
-	function loop() {
-		frame = requestAnimationFrame(loop)
-
-		if (!analyzer || !canvas) return
-
-		const ctx = canvas.getContext('2d')
-
-		let bar_x
-		let bar_width
-		let bar_height
-		let bar_num = 100
-
-		let bufferLength = analyzer.frequencyBinCount
-		let dataArray = new Uint8Array(bufferLength)
-		analyzer?.getByteFrequencyData(dataArray)
-
-		ctx.clearRect(0, 0, canvas.width, canvas.height)
-		ctx.fillStyle = '#000'
-
-		for (let i = 0; i < bar_num; i++) {
-			bar_x = i * 3
-			bar_width = 2
-			bar_height = -(dataArray[i] / 2)
-			ctx.fillRect(bar_x, canvas.height, bar_width, bar_height)
-		}
-	}
-
-	onMount(() => {
-		loop()
-
-		return () => cancelAnimationFrame(frame)
-	})
 </script>
--- src/lib/Frequency.svelte
+++ src/lib/Frequency.svelte
@@ -1,11 +1,42 @@
-<canvas bind:this={elem} />
+<canvas bind:this={canvas} />
 
 <script>
-	let elem
+	import { onMount } from 'svelte'
 
-	$: canvas = elem
-</script>
+	export let analyzer
 
-<script context="module">
-	export let canvas
+	let canvas, frame
+
+	function loop() {
+		frame = requestAnimationFrame(loop)
+
+		if (!analyzer || !canvas) return
+
+		const ctx = canvas.getContext('2d')
+
+		let bar_x
+		let bar_width
+		let bar_height
+		let bar_num = 100
+
+		let bufferLength = analyzer.frequencyBinCount
+		let dataArray = new Uint8Array(bufferLength)
+		analyzer?.getByteFrequencyData(dataArray)
+
+		ctx.clearRect(0, 0, canvas.width, canvas.height)
+		ctx.fillStyle = '#000'
+
+		for (let i = 0; i < bar_num; i++) {
+			bar_x = i * 3
+			bar_width = 2
+			bar_height = -(dataArray[i] / 2)
+			ctx.fillRect(bar_x, canvas.height, bar_width, bar_height)
+		}
+	}
+
+	onMount(() => {
+		loop()
+
+		return () => cancelAnimationFrame(frame)
+	})
 </script>
